74e5b384d6e4cf85240e11cb7afb8f8c607808d6
Audrey Palmore <audreypalmore1@gmail.com>
2022-04-10 19:30:02 -0500

    got it working

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ this service will turn on the light at sunrise using Pi-Relay-Server
 ## Setup
 ## you will need to set your location in conf.py
 ###go to https://gml.noaa.gov/grad/solcalc/ and look at the location field there site helped me figure out my lat/long and utc offset
+
+`sudo cp suntracker.service /etc/systemd/system/` 
+`sudo systemctl daemon-reload`
+`sudo systemctl enable suntracker.service`
+`sudo systemctl start suntracker.service`
+`sudo systemctl status suntracker.service`
--- suntracker.py
+++ suntracker.py
@@ -6,6 +6,8 @@ from datetime import date, timedelta, datetime
 from math import floor
 from time import sleep
 from random import randint
+import requests
+import vlc
 
 class Sun:
     def __init__(self,city,utc_offset):
@@ -57,3 +59,40 @@ class Sun:
     def dusk(self):
         return 0
 
+class AudreysSun(Sun):
+    def  __init__(self):
+        super().__init__(city="Atlanta",utc_offset=-4)
+        self.host = 'http://127.0.0.1:420'
+        self.streams = 'http://uk1.internet-radio.com:4086/listen.pls'
+    def dusk(self):
+
+        #turn on lamp
+        #this makes tells github.com/audrey163/Pi-Relay-Server hosted on my raspberry pi to turn on the lamp
+        url = self.host + '/high?ch=1'
+        response = requests.get(url)
+        print("sun.dusk turn on light [ " + response.text + " ]")
+
+        #radio_bbc = 'http://www.vpr.net/vpr_files/stream_playlists/vpr_bbc_mp3.pls'
+        #self.stream_radio(url=radio_bbc,time=15*60)
+
+    def dawn(self):
+        response = request.get(self.host+'high?ch=1' )
+        sleep(0.5)
+        response = request.get(self.host+'low?ch=1') # I turn the switch on and off to wake up the red bluetooth led lamp
+        sleep(0.5)
+    def stream_radio(self,url,time):
+        instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
+        #Define VLC player
+        player=instance.media_player_new()
+        #Define VLC media
+        media=instance.media_new(url)
+        #Set player media
+        player.set_media(media)
+        #Play the media
+        player.play()
+        sleep(time)
+        player.stop()
+
+if __name__ == '__main__':
+    s = AudreysSun()
+    s.daemon()
