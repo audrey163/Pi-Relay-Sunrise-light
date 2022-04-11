5264417d52681af34bbed1b2f17d4705044a3686
Audrey Palmore <audreypalmore1@gmail.com>
2022-04-10 20:15:24 -0400

    main

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ from time import sleep
 class AudreysSun(Sun):
     def  __init__(self):
         super().__init__(city="Atlanta",utc_offset=-4)
-        self.host = 'http://10.0.0.203:420'
+        self.host = 'http://10.0.0.4:420'
         self.streams = 'http://uk1.internet-radio.com:4086/listen.pls'
     def dusk(self):
 
         #turn on lamp
         #this makes tells github.com/audrey163/Pi-Relay-Server hosted on my raspberry pi to turn on the lamp
-        url = self.host + '/on?ch=1'
+        url = self.host + '/high?ch=1'
         response = requests.get(url)
         print("sun.dusk turn on light [ " + response.text + " ]")
 
-        radio_bbc = 'http://www.vpr.net/vpr_files/stream_playlists/vpr_bbc_mp3.pls'
-        self.stream_radio(url=radio_bbc,time=15*60)
+        #radio_bbc = 'http://www.vpr.net/vpr_files/stream_playlists/vpr_bbc_mp3.pls'
+        #self.stream_radio(url=radio_bbc,time=15*60)
 
+    def dawn(self):
+        response = request.get(self.host+'high?ch=1' )
+        sleep(0.5) 
+        response = request.get(self.host+'low?ch=1') # I turn the switch on and off to wake up the red bluetooth led lamp
+        sleep(0.5)
     def stream_radio(self,url,time):
         instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
         #Define VLC player
@@ -35,4 +40,4 @@ class AudreysSun(Sun):
 if __name__ == '__main__':
     #s.daemon()
     s = AudreysSun()
-    s.dusk()
+    s.daemon()
